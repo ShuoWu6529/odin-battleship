2adbbd29207f3742288adcea572e158c34336fec
Shuo Cao <sw6529@nyu.edu>
2026-06-13 20:30:10 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { testing } from "./export-template.js";
+
+/**
+ * npx eslint dir/file.js
+ * npx prettier . --check
+ * npx prettier . --write
+ * npm run dev
+ * npm run build
+ * npm test
+ **/
+
+console.log(testing);
