1d189f688a3aa1f8268db259c374f28b03982bbd
Shuo Wu <sw6529@nyu.edu>
2026-06-21 00:26:19 -0400

    Complete basic Ship class

---
+++ src/ship.js
@@ -0,0 +1,27 @@
+class Ship {
+
+  #length;
+  #hits = 0
+
+  constructor(length) {
+    this.#length = length;
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  get hits() {
+    return this.#hits;
+  }
+
+  hit() {
+    this.#hits += 1;
+  }
+
+  isSunk() {
+    return this.#hits >= this.#length;
+  }
+}
+
+export { Ship };
---
+++ src/ship.test.js
@@ -0,0 +1,36 @@
+import { experiments } from "webpack";
+import { Ship } from "./ship.js";
+
+let ship;
+
+beforeEach(() => {
+  ship = new Ship(2);
+})
+
+test('Ship Exists', () => {
+  expect(ship).toBeDefined();
+});
+
+test('Get Ship length', () => {
+  expect(ship.length).toBe(2);
+})
+
+test('Get hits', () => {
+  expect(ship.hits).toBe(0);
+})
+
+test('Hit functionality', () => {
+  ship.hit();
+  ship.hit();
+  expect(ship.hits).toBe(2);
+})
+
+test('check ship sunkness', () => {
+  expect(ship.isSunk()).toBeFalsy();
+})
+
+test('check ship sunkness 2', () => {
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBeTruthy();
+})
